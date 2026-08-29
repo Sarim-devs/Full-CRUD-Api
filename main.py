020bf7c4e6c0a6edfece350d6db4d13493da3be4
Sarim <sarimsuman1998@gmail.com>
2026-08-29 22:48:57 +0500

    Stage 3: Creation With Validation

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def health_check():
 @app.get("/tasks")
 def get_tasks():
      return tasks
+@app.post("/tasks",status_code=201)
+def create_task(task: dict):
+    if "title" not in task or not task["title"]:
+     raise HTTPException(status_code=400,detail=f"Title is required")        
+    new_id = max([t["id"] for t in tasks], default=0) + 1
+    new_task = {"id": new_id,"title": task["title"],"done":False}
+    tasks.append(new_task)
+    return new_task
 @app.get("/tasks/{task_id}")
 def get_task(task_id:int):
   for task in tasks:
